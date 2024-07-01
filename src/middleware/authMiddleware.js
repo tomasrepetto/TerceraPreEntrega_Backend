eb8c28866d4846cb32311711cdffeb90568570d9
tomasrepetto <tomasrepetto@gmail.com>
2024-07-01 13:11:49 -0300

    Ultimos detalles

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,29 +1,31 @@
-import { getCartsByIdService } from '../dao/cartsMongo.js';
+import jwt from 'jsonwebtoken';
+import { getUserById } from '../dao/userMongo.js';
 
-export const auth = (req, res, next) => {
-    if (req.isAuthenticated()) {
-        return next();
+export const auth = async (req, res, next) => {
+    const token = req.headers.authorization?.split(' ')[1];
+    if (!token) {
+        res.status(401).json({ message: 'Unauthorized' });
     }
-    res.status(401).json({ message: 'Unauthorized' });
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await getUserById(decoded.id);
+        if(!user) {
+            res.status(401).json({ message: 'Unauthorized' });
+        }
+        req.user = user;
+        next();
+    } catch (error) {
+        res.status(401).json({ message: 'Unauthorized' });
+    }
 };
 
 export const authorize = (roles = []) => {
     return async (req, res, next) => {
-        if (!req.isAuthenticated()) {
+        if (!req.user) {
             return res.status(401).json({ message: 'Unauthorized' });
         }
-        if (!roles.includes(req.user.rol)) {
-            try {
-                const carrito = await getCartsByIdService(req.user.cartId);
-                if (carrito) {
-                    return res.json(carrito);
-                } else {
-                    return res.status(404).json({ message: 'Cart not found' });
-                }
-            } catch (error) {
-                console.error('Error al obtener el carrito del usuario:', error);
-                return res.status(500).json({ message: 'Internal Server Error' });
-            }
+        if (roles.length && !roles.includes(req.user.rol)) {
+            res.status(403).json({ message: 'Forbidden' });
         }
         next();
     };
